3b9ed7f1ef9d66865def0015936451c880d5d9d9
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-03-25 21:56:16 +0100

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { User } from './user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'user',
+    component: User,
+  },
+];
