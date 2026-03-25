e2864e5c51ee585ab076b246ca14ca4f47a28653
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-03-25 21:55:43 +0100

    feat: add logo-container, nav-list and router-outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
