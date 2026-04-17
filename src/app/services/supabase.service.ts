074f564f4a1310c4cbdec199c3934d3ac398d93d
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-04-17 11:39:37 +0200

    feat: add userList signal and gtAllUsers method SupabaseService, modify addUser method

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment';
 import { UserModel } from '../models/user.model';
@@ -10,7 +10,11 @@ export class SupabaseService {
   private supabase = createClient(environment.supabaseURL, environment.supabaseKey);
   userChannels;
 
+  userList = signal<UserModel[]>([]);
+
   constructor() {
+    this.getAllUsers().catch((error) => console.error('Error loading users:', error));
+
     this.userChannels = this.supabase
       .channel('custom-all-channel')
       .on('postgres_changes', { event: '*', schema: 'public', table: 'users' }, (payload) => {
@@ -35,8 +39,15 @@ export class SupabaseService {
     this.supabase.removeChannel(this.userChannels);
   }
 
+  async getAllUsers() {
+    const { data: users, error } = await this.supabase.from('users').select('*');
+    if (error) throw error;
+
+    this.userList.set(users?.map((user) => UserModel.toCamelCase(user)) ?? []);
+  }
+
   async addUser(user: UserModel) {
-    const userData = user.formatForDB();
+    const userData = user.toSnakeCase();
     const { data, error } = await this.supabase.from('users').insert([userData]).select();
 
     if (error) throw error;
