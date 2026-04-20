b8bd6e626dcaf0f72957afacc5cdf98e9a1a9bc3
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-04-20 21:48:20 +0200

    feat: add user detail UI and get user ID from URL

--- src/app/components/user-detail/user-detail.ts
+++ src/app/components/user-detail/user-detail.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [MatCardModule, MatDividerModule],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.scss',
 })
-export class UserDetail {}
+export class UserDetail {
+  private route = inject(ActivatedRoute);
+  private userId: string | null;
+
+  constructor() {
+    this.userId = this.route.snapshot.paramMap.get('id');
+    console.log(this.userId);
+  }
+}
