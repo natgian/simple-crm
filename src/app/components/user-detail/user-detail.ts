de61b01eb6a8a11cd128e117c4309c56b84016cb
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-04-21 21:40:03 +0200

    feat: load and display single user data in UserDetail

--- src/app/components/user-detail/user-detail.ts
+++ src/app/components/user-detail/user-detail.ts
@@ -2,19 +2,28 @@ import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { ActivatedRoute } from '@angular/router';
+import { SupabaseService } from '../../services/supabase.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [MatCardModule, MatDividerModule],
+  imports: [MatCardModule, MatDividerModule, DatePipe],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.scss',
 })
 export class UserDetail {
   private route = inject(ActivatedRoute);
   private userId: string | null;
+  supabaseService = inject(SupabaseService);
+  currentUser = this.supabaseService.user;
 
   constructor() {
     this.userId = this.route.snapshot.paramMap.get('id');
-    console.log(this.userId);
+  }
+
+  ngOnInit() {
+    if (this.userId) {
+      this.supabaseService.getSingleUser(this.userId);
+    }
   }
 }
