5c426b4b47bbca967bf466850aaea4b498750848
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-04-24 10:23:16 +0200

    feat: add editing menus on user-detail and replace SVGs with mat-icons

--- src/app/components/user-detail/user-detail.ts
+++ src/app/components/user-detail/user-detail.ts
@@ -1,17 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import { ActivatedRoute } from '@angular/router';
 import { SupabaseService } from '../../services/supabase.service';
 import { DatePipe } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogEditUserBasics } from '../dialog-edit-user-basics/dialog-edit-user-basics';
+import { DialogEditUserDetails } from '../dialog-edit-user-details/dialog-edit-user-details';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [MatCardModule, MatDividerModule, DatePipe],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatDividerModule,
+    DatePipe,
+  ],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.scss',
 })
 export class UserDetail {
+  readonly dialog = inject(MatDialog);
   private route = inject(ActivatedRoute);
   private userId: string | null;
   supabaseService = inject(SupabaseService);
@@ -25,5 +39,13 @@ export class UserDetail {
     if (this.userId) {
       this.supabaseService.getSingleUser(this.userId);
     }
+  }
+
+  openEditBasicsDialog(): void {
+    this.dialog.open(DialogEditUserBasics);
+  }
+
+  openEditDetailsDialog(): void {
+    this.dialog.open(DialogEditUserDetails);
   }
 }
